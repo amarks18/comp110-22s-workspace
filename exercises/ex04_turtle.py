56890c637e5e1f1fd6130224efbc6b54ed742f9b
Anna Marks <adm18@email.unc.edu>
2022-02-23 22:53:31 -0500

    ex04

--- exercises/ex04_turtle.py
+++ exercises/ex04_turtle.py
@@ -6,29 +6,139 @@ from turtle import Turtle, colormode, done, color
 
 def main() -> None: 
     """The entrypoint of my scene."""
-    mountain()
+    mount: Turtle = Turtle()
+    mountain(mount, -450, -52, 300)
+    mountain(mount, -400, -50, 400)
+    mountain(mount, -200, -40, 300)
+    mountain(mount, -50, -51, 475)
+    grass()
+    sun()
+    ladybug()
+    dot()
     done()
 
-def mountain() -> None: 
+def mountain(mount: Turtle, x: float, y: float, size: float) -> None: 
     """Drawing mountains to include in mountain scene."""
-    mount: Turtle = Turtle()
     sides: int = 0 
-    count: int = 1
-    while count < 4:
-        coord: int = -450
-        #mount.penup()
-        mount.goto(coord, -50)
-        #mount.pendown()       
-        mount.begin_fill()
-        mount.pencolor("#714320")
-        mount.fillcolor("#714320")
-        while sides < 3:
-            mount.forward(450)
-            mount.left(120)
-            sides += 1
-        coord += 150
-        mount.end_fill()
-        count += 1
+    mount.penup()
+    mount.goto(x, y)        
+    mount.pendown()       
+    mount.begin_fill()
+    mount.pencolor("#714320")
+    mount.fillcolor("#714320")
+    while sides < 3:
+        mount.forward(size)
+        mount.left(120)
+        sides += 1
+    mount.end_fill()
+
+def grass() -> None:
+    """Drawing grass."""
+    grass: Turtle = Turtle()
+    sides: int = 0 
+    grass.penup()
+    grass.goto(-450, -50)
+    grass.pendown()
+    grass.begin_fill()
+    grass.pencolor("#207122")
+    grass.fillcolor("#207122")
+    while sides < 4:
+        grass.forward(1000)
+        grass.right(90)
+        sides += 1 
+    grass.end_fill()
+
+def sun() -> None: 
+    """Drawing the sun."""
+    sun: Turtle = Turtle()
+    sun.penup()
+    sun.goto(-100, 200)
+    sun.pendown()
+    sun.pensize(5)
+    sun.begin_fill()
+    sun.pencolor("#F58216")
+    sun.fillcolor("#F5DE16")
+    sun.circle(30)
+    sun.end_fill()
+
+def ladybug() -> None: 
+    """Drawing a ladybug in the grass."""
+    bug: Turtle = Turtle()
+    bug.penup()
+    bug.goto(40, -225)
+    bug.pendown()
+    bug.pensize(10)
+    bug.begin_fill()
+    bug.pencolor("#000000")
+    bug.fillcolor("#D11A1A")
+    bug.circle(75)
+    bug.end_fill()
+    bug.left(90)
+    bug.forward(150)
+    
+def dot() -> None: 
+    dot: Turtle = Turtle()
+    dot.penup()
+    dot.goto(15, -195)
+    dot.pendown()
+    dot.fillcolor("#000000")
+    dot.dot(30)
+    dot.penup()
+    dot.goto(0, -150)
+    dot.pendown()
+    dot.fillcolor("#000000")
+    dot.dot(30)
+    dot.penup()
+    dot.goto(15, -110)
+    dot.pendown()
+    dot.fillcolor("#000000")
+    dot.dot(30)
+    dot.penup()
+    dot.goto(65, -190)
+    dot.pendown()
+    dot.fillcolor("#000000")
+    dot.dot(30)
+    dot.penup()
+    dot.goto(80, -150)
+    dot.pendown()
+    dot.fillcolor("#000000")
+    dot.dot(30)
+    dot.penup()
+    dot.goto(65, -105)
+    dot.pendown()
+    dot.fillcolor("#000000")
+    dot.dot(30)
+
+    # bug.penup()
+    # bug.goto(15, -195)
+    # bug.pendown()
+    # bug.fillcolor("#000000")
+    # bug.dot(30)
+    # bug.penup()
+    # bug.goto(0, -150)
+    # bug.pendown()
+    # bug.fillcolor("#000000")
+    # bug.dot(30)
+    # bug.penup()
+    # bug.goto(15, -110)
+    # bug.pendown()
+    # bug.fillcolor("#000000")
+    # bug.dot(30)
+    # bug.penup()
+    # bug.goto(65, -190)
+    # bug.pendown()
+    # bug.fillcolor("#000000")
+    # bug.dot(30)
+    # bug.penup()
+    # bug.goto(80, -150)
+    # bug.pendown()
+    # bug.fillcolor("#000000")
+    # bug.dot(30)
+    # bug.penup()
+    # bug.goto(65, -105)
+    # bug.pendown()
+    # bug.fillcolor("#000000")
+    # bug.dot(30)
 
 if __name__ == "__main__":
     main()
